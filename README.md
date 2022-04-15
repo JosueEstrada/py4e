d8ae094ea85c69597a21bbc86920f815fd1425a5
Josué Estrada <josue.estrada@unmsm.edu.pe>
2022-04-14 23:18:20 -0500

    Add ex_05_01, README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# py4e
+Exercises from Python for Everybody in www.freecodecamp.org
+
+Scientific Computing with Python 
---
+++ ex_05_01/ex_05_01.py
@@ -0,0 +1,26 @@
+# Ejercicio 1: Escribe un programa que lea repetidamente números hasta que el
+# usuario introduzca “fin”. Una vez se haya introducido “fin”, muestra por
+# pantalla el total, la cantidad de números y la media de esos números. Si el
+# usuario introduce cualquier otra cosa que no sea un número, detecta su
+# fallo usando try y except, muestra un mensaje de error y pasa al número
+# siguiente.
+def ex_05_01():
+    suma = 0
+    contador = 0
+    promedio = 0
+    while True:
+        dato = input("Introduzca un número:")
+        if dato == "fin":
+            break
+        try:
+            dato = int(dato)
+            contador += 1
+            suma += dato
+        except:
+            print("Error: Entrada inválida")
+
+    promedio = suma / contador
+    print(suma, contador, promedio)
+
+
+ex_05_01()

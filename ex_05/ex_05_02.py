ed7b220bba1d55c63a24ca7bd7b110c92d934ee4
Josué Estrada <josue.estrada@unmsm.edu.pe>
2022-04-15 00:08:07 -0500

    Add ex_05_02

---
+++ ex_05/ex_05_02.py
@@ -0,0 +1,23 @@
+# Ejercicio 2: Escribe otro programa que pida una lista de números como
+# la anterior y al final muestre por pantalla el máximo y mínimo de los
+# números, en vez de la media.
+def ex_05_02():
+    max = None
+    min = None
+    while True:
+        num = input("Ingrese un número:")
+        if num == "fin":
+            break
+        try:
+            num = int(num)
+        except:
+            print("Número invalido")
+            continue
+        if min is None or num < min:
+            min = num
+        if max is None or num > max:
+            max = num
+    print(f"Máximo = {max} , Mínimo = {min}")
+
+
+ex_05_02()

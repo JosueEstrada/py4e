5ec58f1e4ac25e21f72d9e0059af741db6cac404
Josué Estrada <josue.estrada@unmsm.edu.pe>
2022-04-15 23:21:09 -0500

    Add ex_06_06

---
+++ ex_06/ex_06_06.py
@@ -0,0 +1,8 @@
+# Ejercicio 6: Lee la documentación de los métodos de cadenas en
+# https://docs.python.org/library/stdtypes.html#string-methods Quizá quieras
+# experimentar con algunos de ellos para asegurarte de entender como
+# funcionan. strip y replace son particularmente útiles. La documentación usa
+# una sintaxis que puede ser confusa. Por ejemplo, en find(sub[, start[,
+# end]]), los corchetes indican argumentos opcionales. De modo que sub es
+# requerido, pero start es opcional, y si se incluye start, entonces end es
+# opcional.
